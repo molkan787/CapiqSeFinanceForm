937a41d095223c0452302da3489d96a7468acb8e
Dahmane Seghier <dahmane.seghier@gmail.com>
2021-06-10 22:28:38 +0100

    Made API endpoint dynamic based on the current website's origin

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,40 +1,44 @@
 const dev = process.env.NODE_ENV !== 'production';
-const API_BASE_URL = dev ? 'http://localhost/api' : 'https://invest.capiq.se/api';
+// const API_BASE_URL = dev ? 'http://localhost/api' : 'https://invest.capiq.se/api';
 
 export class Api{
 
-    public API_BASE_URL: string = API_BASE_URL;
+    // public API_BASE_URL: string = API_BASE_URL;
 
     constructor(private axios: any){}
 
+    public get API_BASE_URL(){
+        return dev ? 'http://localhost/api' : window.location.origin + '/api';
+    }    
+
     public async getInvestmentProposal() {
-        const { data } = await this.axios.get(`${API_BASE_URL}/investmentProposal.php`)
+        const { data } = await this.axios.get(`${this.API_BASE_URL}/investmentProposal.php`)
         return data
     }
 
     public async submitForm(formData: any){
-        const { data } = await this.axios.post(`${API_BASE_URL}/submitForm.php`, formData)
+        const { data } = await this.axios.post(`${this.API_BASE_URL}/submitForm.php`, formData)
         return data
     }
 
     public async getSettings(){
-        const { data } = await this.axios.get(`${API_BASE_URL}/admin_settings.php`)
+        const { data } = await this.axios.get(`${this.API_BASE_URL}/admin_settings.php`)
         return data
     }
 
     public async putSettings(settings: any){
-        return await this.axios.post(`${API_BASE_URL}/admin_settings.php`, settings)
+        return await this.axios.post(`${this.API_BASE_URL}/admin_settings.php`, settings)
     }
 
     public async requestAuthorizationCode(internalRedirect: string){
-        const { data } = await this.axios.post(`${API_BASE_URL}/requestAuthorizationCode.php`, {
+        const { data } = await this.axios.post(`${this.API_BASE_URL}/requestAuthorizationCode.php`, {
             internalRedirect
         })
         return data
     }
 
     public async getThankyouMessage(info: any){
-        const { data } = await this.axios.post(`${API_BASE_URL}/thankyou_message.php`, info)
+        const { data } = await this.axios.post(`${this.API_BASE_URL}/thankyou_message.php`, info)
         return data
     }
 
